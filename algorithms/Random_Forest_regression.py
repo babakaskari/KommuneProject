8863372037b216be11c81a492e02d437b2912425
mohammj <babakas76@gmail.com>
2021-01-18 00:17:48 +0100

    17JAN

--- algorithms/Random_Forest_regression.py
+++ algorithms/Random_Forest_regression.py
@@ -9,7 +9,6 @@ hour = 13
 hour_range = np.arange(10, 12, 1)
 # computation_range = [5, 10, 17, 35, 55, 78, 1002]
 hour_from = np.arange(1, 5, 1)
-
 clf = RandomForestRegressor(max_depth=3, random_state=42, n_jobs=-1)
 data_selector.data_selector(clf, month, day, hour, hour_range, hour_from)
 
